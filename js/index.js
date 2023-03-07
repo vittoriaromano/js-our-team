79058e05b9717186530943c2b407b070bdd4c547
vittoriaromano <vittoria5005@gmail.com>
2023-03-07 19:16:07 +0400

    milestone-0/1

--- js/index.js
+++ js/index.js
@@ -1,1 +1,42 @@
 'use strict'
+//team members
+const teamMember1 ={
+    nome: 'Wayne Barnett',
+    ruolo: 'Founder & CEO',
+    foto : 'wayne-barnett-founder-ceo.jpg'
+}
+
+const teamMember2 ={
+    nome: 'Angela Caroll',
+    ruolo: 'Chief Editor',
+    foto : 'angela-caroll-chief-editor.jpg'
+}
+
+
+const teamMember3 ={
+    nome: 'Walter Gordon',
+    ruolo: 'Office Manager',
+    foto : 'walter-gordon-office-manager.jpg'
+}
+
+const teamMember4 ={
+    nome: 'Angela Lopez',
+    ruolo: 'Social Media Manager',
+    foto : 'angela-lopez-social-media-manager.jpg'
+}
+
+const teamMember5 ={
+    nome: 'Scott Estrada',
+    ruolo: 'Developer',
+    foto : 'scott-estrada-developer.jpg'
+}
+
+const teamMember6 ={
+    nome: 'Barbara Ramos',
+    ruolo: 'Graphic Designer',
+    foto : 'barbara-ramos-graphic-designer.jpg'
+}
+
+//array team members
+ const team =[teamMember1,teamMember2,teamMember3,teamMember4,teamMember5,teamMember6];
+ console.log(team)
